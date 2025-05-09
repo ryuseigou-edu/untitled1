ffc2b93f558077354556c599316e36f9e950779d
unknown <241101>
2025-05-09 11:51:53 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("中曽　琉成");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
