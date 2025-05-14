01253ff1f8d0709e13baef5eb7cf12641f0cda7d
RyuseiNakaso <241101@std.hi-joho.ac.jp>
2025-05-14 10:18:05 +0900

    GitHubにアップロードする手順をデモするためにファイルを変更した

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("中曽　琉成");
+        System.out.printf("中曽 琉成");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
