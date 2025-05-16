15b3a145d345b93cef7bbf1542857b8de44efe9a
RyuseiNakaso <241101@std.hi-joho.ac.jp>
2025-05-16 11:59:51 +0900

    Review12の追加

---
+++ src/Review12.java
@@ -0,0 +1,14 @@
+public class Review12 {
+    public static void main(String[] args){
+        int age = 19;
+        if(age >= 65){
+            System.out.println("高齢者");
+        } else if (age >= 18){
+            System.out.println("成人");
+        } else if (age >= 0) {
+            System.out.println("未成年");
+        } else {
+            System.out.println("無効な年齢です。");
+        }
+    }
+}
