09bab142d6c1f5ffc31fd6ca7913e89813196ede
RyuseiNakaso <241101@std.hi-joho.ac.jp>
2025-05-16 11:44:36 +0900

    Review8の追加

---
+++ src/Review8.java
@@ -0,0 +1,12 @@
+public class Review8 {
+    public static void main(String[] args){
+        int a = 10;
+        double b = a;
+        a = (int) b;
+        a = (int) 100000L;
+        b = 100000L;
+        b = (int) a;
+        float f = 5.94f;
+        short s = 10;
+    }
+}
