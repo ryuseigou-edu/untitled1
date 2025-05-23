ee8b367d52cc2beaf3d236d09624ee62b46e75ff
RyuseiNakaso <241101@std.hi-joho.ac.jp>
2025-05-23 11:46:51 +0900

    Review21の追加

---
+++ src/reviews/reviews2/Review21.java
@@ -0,0 +1,12 @@
+package reviews.reviews2;
+
+public class Review21 {
+    public static void main(String[] args){
+        int num = 10;
+        int sum = 0;
+        for(int i = 2; i <= num; i += 2){
+            sum += i;
+        }
+        System.out.println("結果:" + sum);
+    }
+}
