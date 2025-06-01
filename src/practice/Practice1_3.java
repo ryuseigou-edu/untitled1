2a2c363265cd104a00c54216e1bc78e632a2c010
ryuseigou <241101@std.hi-joho.ac.jp>
2025-06-01 20:58:55 +0900

    Practice1_3の追加

---
+++ src/practice/Practice1_3.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Practice1_3 {
+    public static void main(String[] args) {
+        boolean a = true;
+        char b = '駆';
+        double c = 3.14;
+        long d = 314159265358979L;
+        String e = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    }
+}
