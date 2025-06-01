3df4c0eed8dc5020ea30fcf3f1738a30a03774b6
ryuseigou <241101@std.hi-joho.ac.jp>
2025-06-01 22:40:36 +0900

    Review29の修正

--- src/reviews/reviews3/Review29/Main.java
+++ src/reviews/reviews3/Review29/Main.java
@@ -3,14 +3,14 @@ package reviews.reviews3.Review29;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args){
+        KeyInput keyInput = new KeyInput();
         System.out.print("宛先を入力＞");
-        String to = br.readLine();
+        String to = keyInput.readString();
         System.out.print("件名を入力＞");
-        String subject = br.readLine();
+        String subject = keyInput.readString();
         System.out.print("本文を入力＞");
-        String body = br.readLine();
+        String body = keyInput.readString();
         System.out.println(to + "に以下のメールを送信しました。");
         System.out.println("件名: " + subject);
         System.out.println("本文: " + body);
