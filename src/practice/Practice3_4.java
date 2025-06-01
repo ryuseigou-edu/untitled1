64ce0918f0ffe346de1dd51be386fff650378a9a
ryuseigou <241101@std.hi-joho.ac.jp>
2025-06-01 22:20:19 +0900

    practice3_4の追加

---
+++ src/practice/Practice3_4.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Practice3_4 {
+    public static void main(String[] args) {
+        boolean tenki = true;
+        if(tenki == true){
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        } else {
+            System.out.println("映画を見ます");
+            System.out.println("寝ます");
+        }
+    }
+}
